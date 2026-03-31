c6bcfda95a22cb33bc5e00d8c30a0cbab9194bff
ZahwaNabila <zahwanabila434@gmail.com>
2026-03-31 11:41:54 +0700

    commit5

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -23,4 +23,10 @@ bool cekPrima(int n) {
     if (n <= 1) {
         return false;
     }
+    int i = 2;
+    while (i < n) {
+        if (n % i == 0) {
+            return false;
+        }
+        i++;
 }
