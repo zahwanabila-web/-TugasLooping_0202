c5b9aad8f2d9c0247f9f47c4213bdf1af5c573c1
ZahwaNabila <zahwanabila434@gmail.com>
2026-03-31 11:36:25 +0700

    commit1

---
+++ tugaslooping.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int angka;
+int pilihan;
+
+void tampilkanMenu(){
+     cout << "\n=================================\n";
+    cout << "       MENU CEK BILANGAN\n";
+    cout << "=================================\n";
+}
