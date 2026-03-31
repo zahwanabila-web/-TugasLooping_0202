b42e7823e268511eb90ea76fcdb57f32e29d02f0
ZahwaNabila <zahwanabila434@gmail.com>
2026-03-31 11:37:25 +0700

    commit2

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -8,4 +8,8 @@ void tampilkanMenu(){
      cout << "\n=================================\n";
     cout << "       MENU CEK BILANGAN\n";
     cout << "=================================\n";
+    cout << "1. Cek Bilangan Prima\n";
+    cout << "2. Cek Bilangan Fibonacci\n";
+    cout << "0. Keluar\n";
+    cout << "Masukkan pilihan: ";
 }
